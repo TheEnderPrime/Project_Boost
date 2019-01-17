0e1c25bcd61610e0799b3845fecfb38efe5b84a8
David B <dbaugh.oregon@gmail.com>
2019-01-16 22:15:50 -0800

    Started working on UI

---
+++ Assets/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text timerText;
+	float time;
+	
+	void Start()
+	{
+		TextMeshPro mText = gameObject.GetComponent<TextMeshPro>();
+		time = Time.deltaTime;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timerText.text = "Time: " + time;
+	}
+}
